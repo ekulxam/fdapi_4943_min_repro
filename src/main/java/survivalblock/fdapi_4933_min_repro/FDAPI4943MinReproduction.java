1ce77420110fe0f31aedc3403e2c992891d6f6c8
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-12-08 19:17:31 -0800

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
    (Initial commit)

---
+++ src/main/java/survivalblock/fdapi_4933_min_repro/FDAPI4943MinReproduction.java
@@ -0,0 +1,30 @@
+package survivalblock.fdapi_4933_min_repro;
+
+import com.mojang.serialization.Codec;
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry;
+import net.fabricmc.fabric.api.attachment.v1.AttachmentSyncPredicate;
+import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
+import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("UnstableApiUsage")
+public class FDAPI4943MinReproduction implements ModInitializer {
+	public static final String MOD_ID = "fdapi_4933_min_repro";
+    @SuppressWarnings("unused")
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static final AttachmentType<Integer> DATA = AttachmentRegistry.<Integer>builder()
+            .initializer(() -> 0)
+            .persistent(Codec.INT)
+            .syncWith(PacketCodecs.VAR_INT, AttachmentSyncPredicate.all())
+            .buildAndRegister(Identifier.of(MOD_ID, "data"));
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/survivalblock/fdapi_4933_min_repro/mixin/LivingEntityMixin.java
@@ -0,0 +1,24 @@
+package survivalblock.fdapi_4933_min_repro.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import survivalblock.fdapi_4933_min_repro.FDAPI4943MinReproduction;
+
+@SuppressWarnings("UnstableApiUsage")
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity {
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+	private void maybeCrashPlease(CallbackInfo info) {
+        this.setAttached(FDAPI4943MinReproduction.DATA, this.random.nextBetween(0, 99));
+	}
+}
